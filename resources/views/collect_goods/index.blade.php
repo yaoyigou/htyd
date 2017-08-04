a3e031527b0237036f68768e9a3bb04a1005a183
chunyang <1347978937@qq.com>
2017-08-04 17:37:14 +0800

    细节

--- resources/views/collect_goods/index.blade.php
+++ resources/views/collect_goods/index.blade.php
@@ -27,58 +27,60 @@
                 @slot('right')
 
                 @endslot
-                <table id="se">
-                    <tr>
-                        <th>订单标识</th>
-                        <th>订单号</th>
-                        <th>下单时间</th>
-                        <th>收货人</th>
-                        <th>订单总金额</th>
-                        <th>订单状态</th>
-                        <th> 操作</th>
-                    </tr>
-                    @if(count($result)>0)
-                        @foreach($result as $v)
-                            <tr>
-                                <td>
-                                    @if($v->jnmj>0)
-                                        <a>返</a>
-                                    @elseif($v->is_zq>0)
-                                        <a>账</a>
-                                    @elseif($v->is_separate==1)
-                                        <a>货</a>
-                                    @endif</td>
-                                <td class="nub tb1_td1" data-id="{{$v->order_id}}"><a
-                                            href="{{route('order.show',['id'=>$v->order_id,'is_zq'=>$v->is_zq,'is_separate'=>$v->is_separate])}}">{{$v->order_sn}}</a>
-                                </td>
-                                <td class="date tb1_td2">{{date('Y-m-d H:i:s',$v->add_time)}}</td>
-                                <td class="name tb1_td3">{{$v->consignee}}</td>
-                                <td class="score tb1_td4">{{formated_price($v->goods_amount)}}</td>
-                                <td class="data tb1_td5">
-                                    <span class="no_pay" style="color: #e70000;">@if($v->pay_status==2) 已付款 @else
-                                            未付款 @endif</span>
-                                    <span class="stat"
-                                          style="color:#339900;cursor:pointer;position: relative;padding:0 5px;"
-                                          id="ddgz" dd-url="{{route('order.ddgz')}}">订单跟踪</span>
-                                </td>
-                                <td class="result tb1_td6">&nbsp;
-                                    {!! order_status($v->order_id,$v->order_status,$v->pay_status,$v->shipping_status,'handle') !!}
-                                    <a href="{{route('order.show',['id'=>$v->order_id])}}"
-                                       class="f6">查看订单</a>
-                                    @if($v->mobile_pay<=1)
-                                        <a href=""
-                                           class="f6">再次购买</a>
-                                    @endif
-                                </td>
-                            </tr>
-                        @endforeach
-                    @else
-                        <tr>
-                            <td colspan="7">暂无任何订单！</td>
-                        </tr>
-                    @endif
+                    <div  class="content_box"  >
 
-                </table>
+                        <form action="{{route('user.plBuy')}}" method="get" id="form">
+                            <table class="gwc_tb2">
+                                <tr>
+                                    <th><input type="checkbox" id="Checkbox1" class="allselect"/> 全选 </th>
+                                    <th>商品名称</th>
+                                    <th>生产厂家</th>
+                                    <th>包装单位</th>
+                                    <th>规格</th>
+                                    <th>价格</th>
+                                    <th>采购次数</th>
+                                    <th style="width: 113px;">操作</th>
+                                </tr>
+                                @if(count($pages)>0)
+                                    @foreach($pages->goods as $v)
+                                        <tr id="{{$v->goods_id}}" data-id="{{$v->goods_id}}-1">
+                                            <td class="tb2_td1"><input type="checkbox" value="{{$v->goods_id}}" name="ids[]" dd-id="newslist" /></td>
+                                            <td class="tb2_td2"><a href="{{route('goods.index',['id'=>$v->goods_id])}}"><img src="{{$v->goods_thumb}}" alt="{{$v->goods_name}}" title="{{$v->goods_name}}"/></a> <p class="name">{{str_limit($v->goods_name,10)}}</p></td>
+                                            <td class="tb2_td3" alt="{{$v->sccj}}" title="{{$v->sccj}}">{{str_limit($v->sccj,12)}}</td>
+                                            <td class="tb2_td3">{{str_limit($v->dw,12)}}</td>
+                                            <td class="tb2_td3">{{str_limit($v->spgg,12)}}</td>
+                                            <td class="tb2_td6"><span class="subtotal">@if($user->ls_review==1){{formated_price($v->real_price)}}@else 会员可见 @endif</span></td>
+                                            <td class="tb2_td7">{{$v->cgcs or 0}}次 </td>
+                                            <td class="tb2_td8">
+                                                <a href="javascript:@if($v->is_can_buy==1) addToCart1({{$v->goods_id}},1)@else addToCart2({{$v->goods_id}}) @endif" class="cart"><img title="加入购物车" src="{{path('images/shopping_cart1.jpg')}}"></a>
+                                                <a href="{{route('user.deleteCollect',['id'=>$v->rec_id])}}" onclick="return confirm('您确定要从收藏夹中删除选定的商品吗？')" class="cancel"><img title="取消收藏" src="{{path('images/shopping_del.jpg')}}"></a>
+                                            </td>
+                                        </tr>
+                                    @endforeach
+                                @else
+                                    <tr>
+                                        <td colspan="8">暂无收藏商品！</td>
+                                    </tr>
+                                @endif
+                            </table>
+
+
+                            <div class="control">
+                                <div class="con_left">
+                                    <input type="checkbox" id="Checkbox2" class="allselect"/> 全选
+                                    <input type="submit" id='submit_0' value="加入购物车" class="submit"/>
+                                    <a id='cancel1' class="cancel">取消收藏</a>
+                                    <input type="hidden" value="{{route('user.deleteCollectPl')}}" id="action"/>
+                                </div>
+
+
+
+                            </div>
+                        </form>
+                        @if($pages->lastPage()>0)
+                            {!! pagesView($pages->currentPage(),$pages->lastPage(),3,3,['type'=>$type,'url'=>'user.collectList']) !!}
+                        @endif
+                    </div>
                 {!! $result->links('layouts.page',['show_num'=>2]) !!}
             @endcomponent
             @include('layouts.user_menu')
